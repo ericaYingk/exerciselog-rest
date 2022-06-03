d48d8aeb1c43211af7614eec8b7c848dcf9959fe
ericaYingk <105606583+ericaYingk@users.noreply.github.com>
2022-06-03 12:30:36 -0700

    Update exercises_controller.mjs

--- exercises_controller.mjs
+++ exercises_controller.mjs
@@ -13,6 +13,7 @@ app.use(express.json());
  * Create a new exercise with name, reps, weight, unit, date provided in the body
  */
 app.post('/exercises', (req, res) => {
+    response.set('Access-Control-Allow-Origin', '*');
     exercises.createExercises(req.body.name, req.body.reps, req.body.weight, req.body.unit, req.body.date)
         .then(exercise => {
             res.status(201).json(exercise);
@@ -29,6 +30,7 @@ app.post('/exercises', (req, res) => {
  */
 app.get('/', (req, res) => { res.send('Hello from Express!')});
 app.get('/exercises', (req, res) => {
+    response.set('Access-Control-Allow-Origin', '*');
     let filter ={};
     exercises.findExcercises(filter, '', 0)
         .then(exercises => {
@@ -47,6 +49,7 @@ app.get('/exercises', (req, res) => {
  * all its 5 properties to the values provided in the body.
  */
 app.put('/exercises/:_id', (req, res) => {
+    response.set('Access-Control-Allow-Origin', '*');
     exercises.replaceExercise(req.params._id, req.body.name, req.body.reps, req.body.weight, req.body.unit, req.body.date)
         .then(numUpdated => {
             res.status(200).json({_id:req.params._id, name:req.body.name, reps:req.body.reps, weight:req.body.weight, unit:req.body.unit, date:req.body.date})
@@ -61,6 +64,7 @@ app.put('/exercises/:_id', (req, res) => {
  * Delete the exercise whose id is provided in the query parameters
  */
 app.delete('/exercises/:_id', (req, res) => {
+    response.set('Access-Control-Allow-Origin', '*');
     exercises.deleteById(req.params._id)
         .then(deletedCount => {
             res.status(204).send();
